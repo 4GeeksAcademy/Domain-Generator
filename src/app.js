4ccf06769b1568de7b76834251af066ccba4bf56
kasimkid <107450115+kasimkid@users.noreply.github.com>
2023-07-01 20:39:30 +0000

    test

--- src/app.js
+++ src/app.js
@@ -11,11 +11,14 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let domain = ["pued.es"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let x = 0; x < adj.length; x++) {
       for (let y = 0; y < noun.length; y++) {
-        console.log(`${pronoun[i]}${adj[x]}${noun[y]}`);
+        for (let a = 0; a < domain.length; i++) {
+          console.log(`${pronoun[i]}${adj[x]}${noun[y]}${domain[a]}`);
+        }
       }
     }
   }
